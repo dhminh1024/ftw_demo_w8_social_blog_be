05896fb161b2a0cb363694a05b7f046df15889f3
Minh Do <minhdh@coderschool.vn>
2020-08-27 14:50:39 +0700

    add get blogs

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -1,4 +1,32 @@
 const utilsHelper = require("../helpers/utils.helper");
+const Blog = require("../models/blog");
 const blogController = {};
+
+blogController.getBlogs = async (req, res, next) => {
+  try {
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
+
+    const totalBlogs = await Blog.countDocuments();
+    const totalPages = Math.ceil(totalBlogs / limit);
+    const offset = limit * (page - 1);
+
+    const blogs = await Blog.find()
+      .sort({ createdAt: -1 })
+      .skip(offset)
+      .limit(limit);
+
+    return utilsHelper.sendResponse(
+      res,
+      200,
+      true,
+      { blogs, totalPages },
+      null,
+      ""
+    );
+  } catch (error) {
+    next(error);
+  }
+};
 
 module.exports = blogController;
--- routes/blogApi.js
+++ routes/blogApi.js
@@ -2,4 +2,11 @@ const express = require("express");
 const router = express.Router();
 const blogController = require("../controllers/blogController");
 
+/**
+ * @route GET api/blogs?page=1&limit=10
+ * @description Get blogs with pagination
+ * @access Public
+ */
+router.get("/", blogController.getBlogs);
+
 module.exports = router;
