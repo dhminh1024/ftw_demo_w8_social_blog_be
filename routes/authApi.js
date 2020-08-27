f165c799b70b6042a797bb1b2f4b712c8f99fbf4
Minh Do <minhdh@coderschool.vn>
2020-08-27 14:28:40 +0700

    Login

--- routes/authApi.js
+++ routes/authApi.js
@@ -1,5 +1,21 @@
 const express = require("express");
 const router = express.Router();
+const validators = require("../middlewares/validators");
+const { body } = require("express-validator");
 const authController = require("../controllers/authController");
+
+/**
+ * @route POST api/auth/login
+ * @description Login
+ * @access Public
+ */
+router.post(
+  "/login",
+  validators.validate([
+    body("email", "Invalid email").exists().isEmail(),
+    body("password", "Invalid password").exists().notEmpty(),
+  ]),
+  authController.loginWithEmail
+);
 
 module.exports = router;
